84fd670d4c8a9a641fe4ade390cd93000d09fcf3
Mukesh <mukeshprajapati020196@gmail.com>
2025-10-25 05:51:22 +0530

    Refactor

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000", // local
+    ],
+    credentials: true,
+  })
+);
 
 //Setting up config file
 dotenv.config({ path: "config/config.env" });
@@ -26,24 +34,12 @@ const taskRoute = require("./routes/taskRoute");
 const assigneeRoute = require("./routes/assigneeRoutes");
 
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
-// const productRouter = require("./routes/productRoute");
-// const blogRouter = require("./routes/blogRoute");
-// const categoryRouter = require("./routes/prodcategoryRoute");
-// const blogcategoryRouter = require("./routes/blogCatRoute");
-// const brandRouter = require("./routes/brandRoute");
-// const couponRouter = require("./routes/couponRoute");
 
 app.use("/api/user", authRouter);
 app.use("/api/board", boardRoute);
 app.use("/api/column", columnRoute);
 app.use("/api/task", taskRoute);
 app.use("/api/assign", assigneeRoute);
-// app.use("/api/product", productRouter);
-// app.use("/api/blog", blogRouter);
-// app.use("/api/category", categoryRouter);
-// app.use("/api/blogcategory", blogcategoryRouter);
-// app.use("/api/coupon", couponRouter);
-// app.use("/api/brand", brandRouter);
 
 app.use(notFound);
 app.use(errorHandler);
